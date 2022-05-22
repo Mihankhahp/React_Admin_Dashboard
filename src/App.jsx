4fdc4da9183d57e8ee5f62c8bf29d2a446b216c1
Peyman <mihankhahp@gmail.com>
2022-05-23 00:21:04 +0430

    Updating

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="dixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
-              <button
+              {/* <button
                 type="button"
                 className="text-3xl
                 p-3
@@ -43,7 +43,7 @@ function App() {
                 style={{ background: "blue", borderRadius: "50%" }}
               >
                 <FiSettings />
-              </button>
+              </button> */}
             </TooltipComponent>
           </div>
           {activeMenu ? (
